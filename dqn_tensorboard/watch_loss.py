a20e1f1809ad06281a50638c2ac2535e317473f4
ryuzakizh <shahdhardan@gmail.com>
2022-04-29 02:32:27 +0400

    watching loss of DQN

--- dqn_tensorboard/watch_loss.py
+++ dqn_tensorboard/watch_loss.py
@@ -1,1 +1,11 @@
 import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+loss_values = pd.read_csv("dqn_tensorboard/progress.csv")["train/loss"]
+
+plt.plot(loss_values)
+plt.title('Loss Curve - DQN - hyperparameters: ')
+plt.xlabel('Time Steps')
+plt.ylabel('Loss')
+plt.show()
